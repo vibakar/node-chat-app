76f8d250840c98feca8f76ef07e3237c33912d18
vibakar <vibakar23@gmail.com>
2017-01-07 22:30:03 +0530

    added form

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,24 @@ socket.on('connect',function(){
   console.log('connected to server');
 });
 
-socket.on('newMessage',function(message){
-  console.log('new message',message);
-});
-
 socket.on('disconnect',function(){
   console.log('disconnected from server');
+});
+
+socket.on('newMessage',function(message){
+  console.log('new message',message);
+  var li=jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit',function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from: 'User',
+    text:  $('[name=message]').val()
+  },function() {
+
+  });
+
 });
--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ io.on('connection',(socket)=>{
 
   socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message,callback)=>{
     console.log('created message ',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('message sent');
     // socket.broadcast.emit('newMessage',{
     //     from:message.from,
     //    text:message.text,

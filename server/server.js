52f8ac8c8f321900a45147d2eca8a9146ac53517
vibakar <vibakar23@gmail.com>
2017-01-07 18:18:55 +0530

    added event

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path=require("path");
 const express=require("express");
+const http=require("http");
+const socketIO=require("socket.io");
 
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
@@ -7,6 +9,14 @@ var app=express();
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+var server=http.createServer(app);
+var io=socketIO(server);
+io.on('connection',(socket)=>{
+  console.log('new user connected');
+  socket.on('disconnect',()=>{
+    console.log('user was disconnected');
+  });
+});
+server.listen(port,()=>{
   console.log(`server started on port ${port}`);
 });

dc38a07e923eec3f79cfcfdf26c2f7d5d43748d3
vibakar <vibakar23@gmail.com>
2017-01-08 12:35:59 +0530

    added auto scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,25 @@
 var socket=io();
+
+function scrollToBottom(){
+  //selectors
+  var messages=jQuery('#messages');
+  var newMessage=messages.children('li:last-child');
+  //heights
+  var scrollTop=messages.prop('scrollTop');
+  var clientHeight=messages.prop('clientHeight');
+  var scrollHeight=messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+if(scrollTop+clientHeight+newMessageHeight+lastMessageHeight>=scrollHeight){
+  messages.scrollTop(scrollHeight);
+}
+  console.log(scrollTop);
+  console.log(clientHeight);
+  console.log(scrollHeight);
+}
+
+
 socket.on('connect',function(){
   console.log('connected to server');
 });
@@ -9,25 +30,32 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
   var formattedTime=moment(message.createdAt).format('h:mm a');
-  var li=jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  $('#messages').append(li);
+  var template=jQuery('#message-template').html();
+  var html=Mustache.render(template,{
+    text:message.text,
+    from:message.from,
+    createdAt:formattedTime
+  });
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
     var formattedTime=moment(message.createdAt).format('h:mm a');
-    var li=jQuery('<li></li>');
-    var a=jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from} ${formattedTime}:`);
-    a.attr('href',message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var template=jQuery('#location-message-template').html();
+    var html=Mustache.render(template,{
+      url:message.url,
+      from:message.from,
+      createdAt:formattedTime
+    });
+    jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit',function(e){
   e.preventDefault();
 
-  var messageTextBox=  jQuery('[name=message]');
+  var messageTextBox=jQuery('[name=message]');
   socket.emit('createMessage',{
     from: 'User',
     text:  messageTextBox.val()
